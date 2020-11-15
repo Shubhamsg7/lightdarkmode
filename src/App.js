5cd2df57ee60ac3fdab3f319f737f0d383915e61
Shubhamsg7 <shubhamkumar8028@gmail.com>
2020-11-15 12:04:42 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "../node_modules/materialize-css/dist/css/materialize.min.css";
 
 function App() {
+
+  const getMode = () =>{
+    const initialMode = localStorage.getItem("mode")
+      if(initialMode == null){
+        if(window.matchMedia("(prefers-color-scheme:dark)").matches){
+          return true
+        }else{
+          return false
+        }  
+      }else{
+        return JSON.parse(localStorage.getItem("mode"))
+      }
+  }
+
+  const [dark,setMode]=useState(getMode())
+  useEffect(()=>{
+    localStorage.setItem("mode",JSON.stringify(dark))
+  },[dark])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div className={dark ? "App dark-mode":"App"}>
+        <nav>
+          <div className="nav-wrapper">
+            <div className="switch">
+              <label className="label">
+                Off
+                <input type="checkbox" checked={dark} onChange={()=>setMode(!dark)} />
+                <span className="lever"></span>
+                On
+              </label>
+            </div>
+          </div>
+        </nav>
+        <div className="content">
+          <h1>{dark?"Dark Mode is On" : "Light Mode is On"}</h1>
+          <p>want to see sime magic? press toggle button</p>
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default App;
